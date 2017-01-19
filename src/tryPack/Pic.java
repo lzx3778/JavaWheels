a50172d4afd05d45b2621266b73824a4eba56439
lzx3778 <1249574830@qq.com>
2017-01-19 20:44:35 +0800

    hmmm

--- src/tryPack/Pic.java
+++ src/tryPack/Pic.java
@@ -1,8 +1,14 @@
 package tryPack;
 
+import java.util.Scanner;
+
 public class Pic {
 
 	public static void main(String[] args) {
-		
+		Scanner scanner = new Scanner(System.in);
+		while(scanner.hasNext()){
+			String string = scanner.nextLine();
+			System.out.println(string);
+		}
 	}
 }
