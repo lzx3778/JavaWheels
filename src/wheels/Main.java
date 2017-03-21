c2d107adce4ac98b3b6408ac96dca7fdb279806b
lzx <1249574830@qq.com>
2017-03-21 17:58:38 +0800

    cleaned up a little

--- src/wheels/Main.java
+++ src/wheels/Main.java
@@ -1,10 +1,8 @@
 package wheels;
 
-import singlePrograms.DrawRectTriangle;
-
 public class Main {
 
 	public static void main(String[] args) {
-		DrawRectTriangle draw = new DrawRectTriangle();
+
 	}
 }
